b3bdd9f7330f754a53a27d4c1bb2038ec5e6edbb
Carl Toreborg <114061060+carltoreborg@users.noreply.github.com>
2024-02-09 15:19:47 +0100

    Quest

--- Overbooked/Assets/Scripts/Quest/Quest.cs
+++ Overbooked/Assets/Scripts/Quest/Quest.cs
@@ -3,30 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Quest", menuName = "Assets/Scripts/Quest")]
-public class Quest : ScriptableObject
+[System.Serializable]
+public class Quest
 {
+    public bool isActive;
 
-    [Header("Basic info")]
-    public string questName;
-    public string questDescription;
-    
-
-    [Header("Quest Rewards")]
-    public float coinBaseReward;
-    public float coinRewardMultiplier;
-
-    public bool compleded { get; protected set; }
+    public string title;
+    public string description;
+    public int goldReward;
 
 }
 
-public abstract class QuestGoal
-{
-    protected string description;
-    //public GoalType goalType;
-    public int requiredAmount = 1;
-    public int currentAmount { get; protected set; }
-
-    
-
-}
