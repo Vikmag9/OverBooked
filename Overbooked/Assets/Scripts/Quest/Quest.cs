60710841c99e940f5a6b04d9ce208d2b8e6e647a
XanJan <annamajberger@gmail.com>
2024-02-09 15:12:04 +0100

    Fixed

--- Overbooked/Assets/Scripts/Quest/Quest.cs
+++ Overbooked/Assets/Scripts/Quest/Quest.cs
@@ -1,52 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-[CreateAssetMenu(fileName = "New Quest", menuName = "Quest")];
 
+[CreateAssetMenu(fileName = "New Quest", menuName = "Assets/Scripts/Quest")]
 public class Quest : ScriptableObject
 {
 
     [Header("Basic info")]
     public string questName;
     public string questDescription;
+    
 
     [Header("Quest Rewards")]
     public float coinBaseReward;
     public float coinRewardMultiplier;
 
-    [Header("Quest Goals")]
-    public bool isActive;    {
-        return (currentAmount >= requiredAmount);
-    }
-    public void ItemCollected()
-    {
-        if (goalType == GoalType.Gather)
-        {
-            currentAmount++;
-        }
-    }
-    public void ItemDelivered()
-    {
-        if (goalType == GoalType.Deliver)
-        {
-            currentAmount++;
-        }
-    }
-    
-    public void Deactivate()
-    {
-        isActive = false;
-    }
+    public bool compleded { get; protected set; }
 
 }
 
-public class QuestGoal
+public abstract class QuestGoal
 {
     protected string description;
-    public GoalType goalType;
-    public int requiredAmount;
-    public int currentAmount;
+    //public GoalType goalType;
+    public int requiredAmount = 1;
+    public int currentAmount { get; protected set; }
 
-    public BoxCollider roomCollider;
+    
 
 }
