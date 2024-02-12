6e4d3a36cb426d2ea43b2026343ee12c4377911b
XanJan <annamajberger@gmail.com>
2024-02-12 01:24:56 +0100

    Dosent work right now

---
+++ Overbooked/Assets/ElevatorCall.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ElevatorCall : MonoBehaviour
+{
+    public ElevetorInputMap controller;
+    private InputAction callElevator;
+
+    private GameObject player;
+
+    public ElevatorMovement em;
+
+    private void Awake()
+    {
+        controller = new ElevetorInputMap();
+
+    }
+
+    private void OnEnable()
+    {
+        callElevator = controller.Elevator.Call;
+        callElevator.Enable();
+        
+    }
+
+    private void OnDisable()
+    {
+        callElevator.Disable();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        callElevator.performed += context => em.SetCalledElevator(true);
+        player = other.gameObject;
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        player = null;
+    }
+
+
+
+    public void MovePlayerInElevator(int currentLevel, Vector3 currentPosition)
+    {
+        if (currentLevel == player.GetComponent<PlayerManager>().getPlayerCurrentLevel())
+        {
+            player.transform.position = new Vector3(currentPosition.x, currentPosition.y + 1, currentPosition.z);
+        }
+    }
+
+    public void MovePlayerOutOfElevator(int currentLevel)
+    {
+        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 3f);
+        player.GetComponent<PlayerManager>().setPlayerCurrentLevel(currentLevel);
+
+
+    }
+
+    public GameObject getPlayer() { return player; }    
+}
--- Overbooked/Assets/ElevatorMovement.cs
+++ Overbooked/Assets/ElevatorMovement.cs
@@ -9,9 +9,7 @@ public class ElevatorMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public level[] levelList;
-    public ElevetorInputMap controller;
-    private InputAction elevatorInput;
-    private InputAction callElevator;
+    
     private InputAction moveElevatorUp;
     private InputAction moveElevatorDown;
 
@@ -19,10 +17,13 @@ public class ElevatorMovement : MonoBehaviour
     private Boolean calledElevator = false;
     private Boolean moveUpAFloor = false;
     private Boolean moveDownAFloor = false;
+    private Boolean moving = false;
 
     public int currentLevel;
+    private int beforeLevel;
     private float elevatorSpeed = 100f;
 
+    public ElevatorCall ec;
 
 
     [Serializable]
@@ -42,75 +43,50 @@ public class ElevatorMovement : MonoBehaviour
         }
 
     }
+    public void SetCalledElevator(Boolean newInput)
+    {
+        calledElevator = newInput;
+    }
 
     private void Awake()
     {
-        controller = new ElevetorInputMap();
         currentLevel = levelList[0].getLevelNumber();
+        beforeLevel = currentLevel + 1;
+       
+    }
+
+
+    /*private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            moveElevatorUp.performed += context => moveUpAFloor = true;
+            moveElevatorDown.performed += context => moveDownAFloor = true;
+        }
         
+        
+    }*/
 
-    }
-
-    private void OnEnable()
-    {
-        elevatorInput = controller.Elevator.MoveElevator;
-        callElevator = controller.Elevator.Call;
-        moveElevatorUp = controller.Elevator.MoveElevatorUp;
-        moveElevatorDown = controller.Elevator.MoveElevatorDown;
-
-        elevatorInput.Enable();
-        callElevator.Enable();
-        moveElevatorUp.Enable();
-        moveElevatorDown.Enable();
-
-        callElevator.performed += context => calledElevator = true;
-
-
-    }
-
-    private void OnDisable()
-    {
-        elevatorInput.Disable();
-        callElevator.Disable();
-        moveElevatorUp.Disable();
-        moveElevatorDown.Disable();
-    }
     private void OnTriggerStay(Collider other)
     {
-        moveElevatorUp.performed += context => moveUpAFloor = true;
-        moveElevatorDown.performed += context => moveDownAFloor = true;
-        
+        if (other.CompareTag("Player"))
+        {
+
+            MoveElevatorUp();
+        }
     }
-    private void OnTriggerExit(Collider other) 
-    {
-        moveElevatorUp.canceled += context => moveUpAFloor = false;
-        moveElevatorDown.canceled += context => moveDownAFloor = false;
-    }
+
+
+
 
     private void Update()
     {
         //if(transform.position.y < levelList[currentLevel + 1].getLevelPos().position.y)
             
-        moveDirection = elevatorInput.ReadValue<Vector2>();
+        //moveDirection = elevatorInput.ReadValue<Vector2>();
         CallElevator();
         MoveElevatorUp();
-        MoveElevatorDown();
-
-        if (moveUpAFloor)
-        {
-            moveDownAFloor = false;
-            calledElevator = false;
-        }
-        if (moveDownAFloor)
-        {
-            moveUpAFloor = false;
-            calledElevator = false;
-        }
-        if (calledElevator)
-        {
-            moveUpAFloor = false;
-            moveDownAFloor = false;
-        }
+ 
 
 
 
@@ -132,15 +108,19 @@ public class ElevatorMovement : MonoBehaviour
 
     void MoveElevatorUp()
     {
-        if (transform.position.y < levelList[currentLevel + 1].getLevelPos().position.y && moveUpAFloor)
+        if (transform.position.y < levelList[currentLevel].getLevelPos().position.y && moveUpAFloor)
         {
+            moving = true;
             transform.Translate(0, 0.01f * elevatorSpeed * Time.deltaTime, 0);
         }
 
-        if (transform.position.y > levelList[currentLevel + 1].getLevelPos().position.y - 0.1f && moveUpAFloor)
+        if (transform.position.y > levelList[currentLevel].getLevelPos().position.y - 0.1f && moveUpAFloor)
         {
+            moving = false;
             moveUpAFloor = false;
-            transform.position = new Vector3(0, levelList[currentLevel + 1].getLevelPos().position.y, 0);
+            currentLevel += 1;
+            transform.position = new Vector3(transform.position.x, levelList[currentLevel].getLevelPos().position.y, transform.position.z);
+            ec.MovePlayerOutOfElevator(currentLevel);
             
         }
 
@@ -148,15 +128,17 @@ public class ElevatorMovement : MonoBehaviour
 
     void MoveElevatorDown()
     {
-        if (transform.position.y > levelList[currentLevel].getLevelPos().position.y && moveDownAFloor)
+        if (transform.position.y > levelList[currentLevel].getLevelPos().position.y)
         {
+            moving = true;
             transform.Translate(0, -0.01f * elevatorSpeed * Time.deltaTime, 0);
         }
 
         if (transform.position.y < levelList[currentLevel].getLevelPos().position.y + 0.1f && moveDownAFloor)
         {
+            moving = false;
             moveDownAFloor = false;
-            transform.position = new Vector3(0, levelList[currentLevel].getLevelPos().position.y, 0);
+            transform.position = new Vector3(transform.position.x, levelList[currentLevel].getLevelPos().position.y, transform.position.y);
             
         }
 
@@ -164,17 +146,48 @@ public class ElevatorMovement : MonoBehaviour
 
     void CallElevator()
     {
+
         //Get posistion of player who called the elevator and then decide what way
-        if(transform.position.y > levelList[currentLevel].getLevelPos().position.y && calledElevator)
+        //if(ec.getPlayer().GetComponent<PlayerManager>().getPlayerCurrentLevel() < currentLevel)
+        if (currentLevel < beforeLevel)
         {
-            transform.Translate(0, -0.01f * elevatorSpeed * Time.deltaTime, 0);
+            if(transform.position.y > levelList[currentLevel].getLevelPos().position.y && calledElevator)
+            {
+                moving = true;
+                transform.Translate(0, -0.01f * elevatorSpeed * Time.deltaTime, 0);
+            }
+
+            if (transform.position.y - 0.1f <= levelList[currentLevel].getLevelPos().position.y && calledElevator)
+            {
+                moving = false;
+                calledElevator = false;
+                ec.MovePlayerInElevator(currentLevel, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+                moveUpAFloor = true;
+                beforeLevel = currentLevel;
+                currentLevel = beforeLevel - 1;
+            }
+        }
+        else
+        {
+            if (transform.position.y < levelList[currentLevel].getLevelPos().position.y && calledElevator)
+            {
+                moving = true;
+                transform.Translate(0, +0.01f * elevatorSpeed * Time.deltaTime, 0);
+            }
+
+            if (transform.position.y + 0.1f >= levelList[currentLevel].getLevelPos().position.y && calledElevator)
+            {
+                moving = false;
+                calledElevator = false;
+                ec.MovePlayerInElevator(currentLevel, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+                moveDownAFloor = true;
+                currentLevel = beforeLevel;
+                beforeLevel = currentLevel - 1;
+            }
         }
 
-        if (transform.position.y - 0.1f <= levelList[currentLevel].getLevelPos().position.y && calledElevator)
-        {
-            calledElevator = false;
-        }
-        
     }
+
+    
 
 }
